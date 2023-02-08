0e41291d99d60a6edd3a11bb70336aa813a2c83d
Danilo Miranda <danilomirandaa.15@outlook.com>
2023-02-08 20:29:15 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Dan on 08/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
